b10ae0daaf58731849e8609aa67d0ef03c3bbde4
Koosha <i@koosha.io>
2020-04-10 12:54:28 +0200

    rename Factory to KonfigurationFactory.

---
+++ src/main/java/io/koosha/konfiguration/impl/v0/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This is an internal implementation. To get an instance, use
+ * {@link io.koosha.konfiguration.KonfigurationFactory#getInstanceV8()}
+ */
+@ApiStatus.Internal
+package io.koosha.konfiguration.impl.v0;
+
+import org.jetbrains.annotations.ApiStatus;
