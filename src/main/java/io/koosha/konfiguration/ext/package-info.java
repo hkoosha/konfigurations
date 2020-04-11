15c36681c72987c89ec7b3146993a35a024a49d4
Koosha <i@koosha.io>
2020-04-11 09:35:37 +0200

    .

---
+++ src/main/java/io/koosha/konfiguration/ext/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains exceptions related to external projects (external
+ * and extension configuration sources, such as sources for json, yaml, ...).
+ */
+package io.koosha.konfiguration.ext;
--- src/main/java/io/koosha/konfiguration/impl/v8/package-info.java
+++ src/main/java/io/koosha/konfiguration/impl/v8/package-info.java
@@ -1,6 +1,10 @@
 /**
  * This is an internal implementation. To get an instance, use
  * {@link io.koosha.konfiguration.KonfigurationFactory#getInstanceV8()}
+ *
+ * This package hides all implementing classes, and only exposes the
+ * {@link io.koosha.konfiguration.impl.v8.FactoryV8}, which yet, shouldn't
+ * be accessed directly.
  */
 @ApiStatus.Internal
 package io.koosha.konfiguration.impl.v8;
